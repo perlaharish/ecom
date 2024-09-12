1339a0765db82519c4cbccc86fffdd7e4c9bbbbc
perlaharish <perlaharish458@gmail.com>
2024-09-12 11:47:01 +0530

    updated front end script - copy cmd

--- frontend.sh
+++ frontend.sh
@@ -1,10 +1,8 @@
-cp roboshop.conf /etc/nginx/default.d/roboshop.conf
 dnf install nginx -y
 systemctl enable nginx
-systemctl start nginx
+cp roboshop.conf /etc/nginx/default.d/roboshop.conf
 rm -rf /usr/share/nginx/html/*
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
-systemctl enable nginx
 systemctl restart nginx
