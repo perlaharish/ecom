deec628f8f36dd81d7bc3b36693eb56fc2a573c9
perlaharish <perlaharish458@gmail.com>
2024-09-17 12:25:36 +0530

    updated cart service file removal

--- cart.sh
+++ cart.sh
@@ -4,6 +4,7 @@ dnf module disable nodejs -y
 dnf module enable nodejs:18 -y
 dnf install nodejs -y
 useradd roboshop
+rm -rf /app
 mkdir /app
 curl -L -o /tmp/cart.zip https://roboshop-artifacts.s3.amazonaws.com/cart.zip
 cd /app
