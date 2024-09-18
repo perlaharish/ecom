c605d0688439cbec25bb80d0115e9d8f92569419
perlaharish <perlaharish458@gmail.com>
2024-09-18 12:14:33 +0530

    update auto replace of localhost ip in mongo and redis conf files

--- catalogue.sh
+++ catalogue.sh
@@ -1,30 +1,30 @@
 echo -e "#########copy the repo files #########"
-cp catalogue.service /etc/systemd/system/catalogue.service
-cp mongo.repo /etc/yum.repos.d/mongo.repo
+cp catalogue.service /etc/systemd/system/catalogue.service &>> /tmp/catalogue.log
+cp mongo.repo /etc/yum.repos.d/mongo.repo &>> /tmp/catalogue.log
 echo -e "#########Reload daemon service #########"
-systemctl daemon-reload
-dnf module disable nodejs -y
-dnf module enable nodejs:18 -y
+systemctl daemon-reload  &>> /tmp/catalogue.log
+dnf module disable nodejs -y &>> /tmp/catalogue.log
+dnf module enable nodejs:18 -y &>> /tmp/catalogue.log
 echo -e "#########Install nodejs #########"
-dnf install nodejs -y
+dnf install nodejs -y &>> /tmp/catalogue.log
 echo -e "#########user add #########"
-useradd roboshop
+useradd roboshop &>> /tmp/catalogue.log
 echo -e "#########copy the code files #########"
-rm -rf /app
-mkdir /app
-curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
-cd /app
+rm -rf /app &>> /tmp/catalogue.log
+mkdir /app &>> /tmp/catalogue.log
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip &>> /tmp/catalogue.log
+cd /app &>> /tmp/catalogue.log
 echo -e "#########unzip the repo files #########"
-unzip /tmp/catalogue.zip
+unzip /tmp/catalogue.zip &>> /tmp/catalogue.log
 echo -e "#########Install app dependent files #########"
 
-npm install
+npm install &>> /tmp/catalogue.log
 echo -e "#########Restart catalogue service #########"
-systemctl enable catalogue
-systemctl restart catalogue
+systemctl enable catalogue &>> /tmp/catalogue.log
+systemctl restart catalogue &>> /tmp/catalogue.log
 echo -e "#########Install mongo shell #########"
 
-dnf install mongodb-org-shell -y
+dnf install mongodb-org-shell -y &>> /tmp/catalogue.log
 echo -e "#########schema load to mongo db #########"
-mongo --host 172.31.42.57 </app/schema/catalogue.js
+mongo --host 172.31.42.57 </app/schema/catalogue.js &>> /tmp/catalogue.log
 
