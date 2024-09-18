0b766d91f7ee645156ace3ab949cc7aea93ffdbe
perlaharish <perlaharish458@gmail.com>
2024-09-18 11:33:02 +0530

    Remove app directory from payment and dispatch

--- dispatch.sh
+++ dispatch.sh
@@ -2,6 +2,7 @@ cp dispatch.service /etc/systemd/system/dispatch.service
 systemctl daemon-reload
 dnf install golang -y
 useradd roboshop
+rm -rf /app
 mkdir /app
 curl -L -o /tmp/dispatch.zip https://roboshop-artifacts.s3.amazonaws.com/dispatch.zip
 cd /app
