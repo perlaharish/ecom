1ef7e1bcfcefababa12d1b21a1ef0fe8d3e7bf09
perlaharish <perlaharish458@gmail.com>
2024-09-25 12:54:33 +0530

    updated exit status for all services

--- comman.sh
+++ comman.sh
@@ -1,20 +1,31 @@
 log=/tmp/roboshop.log
+func_exist_status(){
+  if [ $? -eq 0 ];then
+    echo -e "\e[32mSuessfull\e[0m"
+ else
+   "\e[31mFailed\e[0m"
+   fi
+}
 func_prereq(){
 echo -e "\e[32m#########user add #########\e[0m" 
 useradd roboshop &>> ${log}
+func_exist_status
 echo -e "\e[32m#########copy the code files #########\e[0m"
 rm -rf /app &>> ${log}
 mkdir /app &>> ${log}
 curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip &>> ${log}
 cd /app &>> ${log}
+func_exist_status
 echo -e "\e[32m#########unzip the repo files #########\e[0m"
 unzip /tmp/${component}.zip &>> ${log}
 }
+func_exist_status
 
 func_servicerestart(){
   echo -e "\e[32m#########Restart ${component} service #########\e[0m"  
   systemctl enable ${component} &>> ${log}
   systemctl restart ${component} &>> ${log}
+  func_exist_status
 }
 
 func_schema_setup(){
@@ -32,23 +43,26 @@ func_schema_setup(){
       mysql -h 172.31.47.181 -uroot -pRoboShop@1 < /app/schema/${component}.sql
       echo -e "\e[32m#########restart service #########\e[0m"
     fi
-
+func_exist_status
 }
 
 func_nodejs(){
 echo -e "\e[32m#########copy the service files####[0m"
 cp ${component}.service /etc/systemd/system/${component}.service &>> ${log}
 cp mongo.repo /etc/yum.repos.d/mongo.repo &>> ${log}
+func_exist_status
 echo -e "\e[32m#########Reload daemon service #########\e[0m"  
 systemctl daemon-reload  &>> ${log}
 dnf module disable nodejs -y &>> ${log}
 dnf module enable nodejs:18 -y &>> ${log}
+func_exist_status
 echo -e "\e[32m#########Install nodejs #########\e[0m"  
 dnf install nodejs -y &>> ${log}
 func_prereq
+func_exist_status
 echo -e "\e[32m#########Install app dependent files #########\e[0m"
 npm install &>> ${log}
-
+func_exist_status
 func_servicerestart
 func_schema_setup
 
@@ -57,14 +71,18 @@ func_schema_setup
 func_python(){
   echo -e "\e[32m#########copy service file #########\e[0m"
   cp ${component}.service /etc/systemd/system/${component}.service
+  func_exist_status
   echo -e "\e[32m#########reload daemon service #########\e[0m"
   systemctl daemon-reload
+  func_exist_status
   echo -e "\e[32m#########Install python #########\e[0m"
   dnf install python36 gcc python3-devel -y
+  func_exist_status
   echo -e "\e[32m#########copy code files and unzip#########\e[0m"
   func_prereq
   echo -e "\e[32m#########Install python dependencies #########\e[0m"
   pip3.6 install -r requirements.txt
+  func_exist_status
   echo -e "\e[32m#########Restart services #########\e[0m"
   func_servicerestart
 }
@@ -72,15 +90,19 @@ func_python(){
 func_java(){
   echo -e "\e[32m#########copy daemon files #########\e[0m"
   cp ${component}.service /etc/systemd/system/${component}.service
+  func_exist_status
   echo -e "\e[32m#########reload daemon service #########\e[0m"
   systemctl daemon-reload
+  func_exist_status
   echo -e "\e[32m#########Install maven #########\e[0m"
   dnf install maven -y
+  func_exist_status
   echo -e "\e[32m#########copy code and unzip #########\e[0m"
   func_prereq
   echo -e "\e[32m########build maven package #########\e[0m"
   mvn clean package
   mv target/${component}-1.0.jar ${component}.jar
+  func_exist_status
 
   func_schema_setup
 
