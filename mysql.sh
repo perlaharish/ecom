2dc4459792ebc84d540d688916199dfe7432e988
perlaharish <perlaharish458@gmail.com>
2024-09-26 10:19:55 +0530

    update mysql password logic in mysql install file

--- mysql.sh
+++ mysql.sh
@@ -1,7 +1,11 @@
+mysql_root_pass=$1
 cp mysql.repo /etc/yum.repos.d/mysql.repo
-
+if [ -z ${mysql_root_pass} ]; then
+  echo "Imported password missing"
+  exit
+  fi
 dnf module disable mysql -y
 dnf install mysql-community-server -y
 systemctl enable mysqld
 systemctl restart mysqld
-mysql_secure_installation --set-root-pass RoboShop@1
+mysql_secure_installation --set-root-pass ${mysql_root_pass}
