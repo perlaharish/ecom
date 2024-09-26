efa7a0cf3ea7a78751c032071b6e297bb133e052
perlaharish <perlaharish458@gmail.com>
2024-09-26 10:55:51 +0530

    add print statements in frontend conf

--- frontend.sh
+++ frontend.sh
@@ -1,8 +1,14 @@
+echo -e "\e[32m#########Insatll Nginx #########\e[0m"
 dnf install nginx -y
+echo -e "\e[32m#########copy roboshop conf file#########\e[0m"
 cp roboshop.conf /etc/nginx/default.d/roboshop.conf
+echo -e "\e[32m#########empaty app folder #########\e[0m"
 rm -rf /usr/share/nginx/html/*
+echo -e "\e[32m#########Download app content #########\e[0m"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
 cd /usr/share/nginx/html
+echo -e "\e[32m#########extract app content #########\e[0m"
 unzip /tmp/frontend.zip
+echo -e "\e[32m#########Restart nginx service #########\e[0m"
 systemctl enable nginx
 systemctl restart nginx
